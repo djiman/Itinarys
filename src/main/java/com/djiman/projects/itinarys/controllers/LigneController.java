0db6e6a90d63a56c31e99d5d3c5c3eb0d83d3a53
Initial Commit <gorguindong@hotmail.fr>
2018-11-10 08:56:08 +0100

    Itinarys : Ajout service rest recherche ligne par nom

--- src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
+++ src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
@@ -6,10 +6,13 @@ package com.djiman.projects.itinarys.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.djiman.projects.itinarys.model.Ligne;
 import com.djiman.projects.itinarys.persistence.LigneRepository;
+import com.djiman.projects.itinarys.persistence.LigneRepositoryCustom;
+import com.djiman.projects.itinarys.util.ConstantesMessages;
 
 /**
  * @author gorguindong
@@ -22,8 +25,22 @@ public class LigneController {
 	@Autowired
 	LigneRepository ligneRepository;
 	
+	@Autowired
+	LigneRepositoryCustom ligneRepositoryCustom;
+
 	@RequestMapping("/ligne")
-    public Iterable<Ligne> getGare() {
+	public Iterable<Ligne> getAllLignes() {
 		return ligneRepository.findAll();
+    }
+
+	@RequestMapping("/ligneByName")
+	public Object getLigneByName(@RequestParam(value = "nomLigne") String nomLigne) {
+		Object result = null;
+		try {
+			result = ligneRepositoryCustom.getLigneByName(nomLigne);
+		} catch (Exception e) {
+			result = ConstantesMessages.AUCUNE_LIGNE_TROUVEE;
+		}
+		return result;
     }
 }
---
+++ src/main/java/com/djiman/projects/itinarys/util/ConstantesMessages.java
@@ -0,0 +1,9 @@
+package com.djiman.projects.itinarys.util;
+
+/**
+ * @author gorguindong
+ *
+ */
+public class ConstantesMessages {
+	public static final String AUCUNE_LIGNE_TROUVEE = "Aucune ligne trouvee";
+}
