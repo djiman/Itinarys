6a19f68df11540d7a63938b9ee5313f7b418db10
Initial Commit <gorguindong@hotmail.fr>
2020-04-17 19:29:05 +0200

    Maj service recuperation ligne

--- src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
+++ src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
@@ -8,10 +8,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.djiman.projects.itinarys.dto.LigneDTO;
@@ -46,8 +46,8 @@ public class LigneController {
 		return (List<LigneDTO>) ligneManager.getAllLignes();
 	}
 
-	@RequestMapping("/ligneByName")
-	public LigneDTO getLigneByName(@RequestParam(value = "nomLigne") String nomLigne) {
+	@RequestMapping("/lignes/{nomLigne}")
+	public LigneDTO getLigneByName(@PathVariable(value = "nomLigne") String nomLigne) {
 		return ligneManager.getLigneByName(nomLigne);
 	}
 
