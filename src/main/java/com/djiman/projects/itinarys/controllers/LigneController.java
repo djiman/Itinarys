9910b9c0616c7bd52f4d31147c82b7dfdf53155c
Initial Commit <gorguindong@hotmail.fr>
2020-05-20 11:39:58 +0200

    Delete cross origin conf

--- src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
+++ src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +24,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
  * @author gorguindong Initial version 1.0.0
  */
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class LigneController {
 
