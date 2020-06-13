5237a621733e87eb88011a686ba9c602b663dad4
Initial Commit <gorguindong@hotmail.fr>
2020-06-13 14:58:24 +0200

    Add Swagger support

--- src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
+++ src/main/java/com/djiman/projects/itinarys/controllers/LigneController.java
@@ -5,6 +5,8 @@ package com.djiman.projects.itinarys.controllers;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -41,16 +43,25 @@ public class LigneController {
 	@Autowired
 	LigneRepositoryCustom ligneRepositoryCustom;
 
+	@ApiOperation(value = "get Lignes",notes = "Recupere toutes les lignes")
 	@RequestMapping("/lignes")
 	public List<LigneDTO> getAllLignes() {
 		return (List<LigneDTO>) ligneManager.getAllLignes();
 	}
 
+	@ApiOperation(value = "get Ligne by name",notes = "Recupere une ligne avec comme paramètre le nom de la ligne")
+	@ApiParam(
+			name =  "${LigneController.getLigneByName.nomLigne}",
+			type = "String",
+			value = "Nom de la ligne",
+			example = "PTB",
+			required = true)
 	@RequestMapping("/lignes/{nomLigne}")
 	public LigneDTO getLigneByName(@PathVariable(value = "nomLigne") String nomLigne) {
 		return ligneManager.getLigneByName(nomLigne);
 	}
 
+	@ApiOperation(value = "create Ligne",notes = "Créer une nouvelle ligne")
 	@RequestMapping(value = "/createLigne", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	void createOrModifyLigne(@RequestBody LigneDTO pLigneDto) throws JsonProcessingException {
 		ligneManager.createOrModifyLigne(pLigneDto);
